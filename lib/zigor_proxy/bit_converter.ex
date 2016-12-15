a30f7a9ddae215bcbb23a6dc80697ef149241cbd
alisinabh <alisina.bm@gmail.com>
2016-12-15 20:33:47 +0330

    BitConverter Parameters docs added

--- lib/zigor_proxy/bit_converter.ex
+++ lib/zigor_proxy/bit_converter.ex
@@ -10,6 +10,9 @@ defmodule ZigorProxy.BitConverter do
   Converts binary of 4 bytes into an signed integer.
   First byte is considered as signature of number.
 
+  ## Parameters:
+    - arg1: a 32-bit integer in form of binary to be decoded to number
+
   ## Examples
     iex> ZigorProxy.BitConverter.get_int32(<<255,255,255,255>>)
     -1
@@ -29,7 +32,10 @@ defmodule ZigorProxy.BitConverter do
   Converts a number into binary.
   this function handles negative and positive numbers.
 
-  ### Examples
+  ## Parameters
+    - number: an integer number not exceeding max signed int32-max and not below int32-min
+
+  ## Examples
     iex> ZigorProxy.BitConverter.int32_bytes(-1)
     <<255,255,255,255>>
 
