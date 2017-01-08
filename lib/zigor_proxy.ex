08f0cbd089c0c65980b84cf476f5b141b3eaf47f
alisinabh <alisina.bm@gmail.com>
2017-01-09 03:02:40 +0330

    SSL support added in ZigorProxy. not tested

--- lib/zigor_proxy.ex
+++ lib/zigor_proxy.ex
@@ -27,8 +27,8 @@ defmodule ZigorProxy do
     case bind do
       {:zigcrypt, ip, port, server_ip, server_port} ->
         do_get_bindings(tail, [worker(Task, [ZigorProxy.Server, :start_listen, [port, ip, server_port, server_ip]]) | final_binds])
-      {:sslcrypt, ip, port, server_ip, server_port} ->
-        do_get_bindings(tail, [worker(Task, [ZigorProxy.Server, :start_listen_ssl, [port, ip, server_port, server_ip]]) | final_binds])
+      {:sslcrypt, ip, port, server_ip, server_port, cert, key} ->
+        do_get_bindings(tail, [worker(Task, [ZigorProxy.Server, :start_listen_ssl, [port, ip, server_port, server_ip, cert, key]]) | final_binds])
       _ -> nil
     end
   end
