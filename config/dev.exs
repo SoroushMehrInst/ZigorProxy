330005d4fbfed3705c06c33d96e311cfa75d174c
alisinabh <alisina.bm@gmail.com>
2017-07-02 21:15:20 +0430

    Fix test config

--- config/dev.exs
+++ config/dev.exs
@@ -4,3 +4,5 @@ config :zigor_proxy, bindings: [
   {:sslcrypt, {192, 168, 1, 244}, 8082, 'origin', 443, 'pem file address', 'key file address', :mnmssl},
   {:zigcrypt, {192, 168, 1, 244}, 8442, 'origin', 901, :mnmzig}
 ]
+
+config :logger, level: :debug
