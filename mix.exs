fadabe59bde6a168660cbab3ad1e1cbc459f4818
Alisina Bahadori <alisina.bm@gmail.com>
2017-07-04 05:00:54 +0430

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ZigorProxy.Mixfile do
 
   def project do
     [app: :zigor_proxy,
-     version: "0.1.6",
+     version: "0.2.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
